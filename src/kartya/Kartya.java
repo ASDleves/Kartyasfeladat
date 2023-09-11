5f629fd4416a9cd9cd9753d4b20798fd731374be
Vörös Dominik <voros.dominik@dk.akkszalezi.hu>
2023-09-11 08:51:32 +0200

    bekérés

--- src/kartya/Kartya.java
+++ src/kartya/Kartya.java
@@ -36,7 +36,7 @@ public class Kartya {
 
         int i = 0;
         for (String tomb1 : tomb) {
-            System.out.print(tomb1+" ");
+            System.out.print(tomb1+"    ");
             i++;
             if (i == 3) {
                 System.out.println("");
@@ -51,8 +51,17 @@ public class Kartya {
     private static void Melyik() {
         Scanner beker = new Scanner(System.in);
         int szam;
-        System.out.println("irj egy lapot!: ");
+        boolean helyes=false;
+        while (helyes!=true) {            
+            
+        
+        System.out.println("Melyik oszlop (1-3)?");
         szam = beker.nextInt();
+        if (szam<0 || szam>4) {
+            System.out.println("Hibás számot adtál meg");
+            helyes=true;
+        }
+        }
         //teszt sor
     }
 
